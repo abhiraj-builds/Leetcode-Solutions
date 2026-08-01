324048442ec899d2f2e302ce6357ed684f71782b
abhiraj-builds <abhirajkumar80846@gmail.com>
2026-08-01 13:18:32 +0530

    Improve LeetCode 112 Path Sum solution

--- Trees/TreeNode.java
+++ Trees/TreeNode.java
@@ -5,6 +5,20 @@ Problem: 112. Path Sum
 Language: Java
 */
 
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+  }
+ 
+
 class Solution {
 
     public boolean hasPathSum(TreeNode root, int targetSum) {
