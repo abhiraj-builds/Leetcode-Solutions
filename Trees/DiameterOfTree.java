62de74ad9f13753b1cea31353ca8a508e70e5fd3
abhiraj-builds <abhirajkumar80846@gmail.com>
2026-08-04 19:44:42 +0530

    solved Diameter of Binary Tree  leetcode problem

---
+++ Trees/DiameterOfTree.java
@@ -0,0 +1,17 @@
+package Trees;
+
+class Solution {
+    public int levels(TreeNode root, int[] maxDia){
+        if(root == null) return 0;
+        int leftLevels = levels(root.left, maxDia);
+        int rightLevels = levels(root.right, maxDia);
+        int dia = leftLevels + rightLevels;
+        maxDia[0] = Math.max(maxDia[0], dia);
+        return 1 + Math.max(leftLevels, rightLevels);
+    }
+    public int diameterOfBinaryTree(TreeNode root) {
+        int [] maxDia = {0};
+        levels(root, maxDia);
+        return maxDia[0];    
+    }
+}
